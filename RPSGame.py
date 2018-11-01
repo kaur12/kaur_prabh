14bf63519bef5c30d4834ade6676c5247294bc85
Kaur <p_kaur92032@tss-hr420-dm06.fc.ca>
2018-11-01 13:17:02 -0400

    adding lives

--- RPSGame.py
+++ RPSGame.py
@@ -4,6 +4,9 @@ from random import randint
 choises = ["Rock", "Paper", "Scissors"]
 player = False
 
+computer_lives = "3"
+player_lives = "3"
+
 # make the computer pick one item at random
 computer = choises[randint(0, 2)]
 
@@ -22,25 +25,39 @@ while player is False:
         if computer == "Paper":
             #computer won
             print("You lose", computer, "covers", player)
+            player_lives == "player_lives - 1"
         else:
             print("You win!", player, "smahes", computer)
+            computer_lives == "computer_lives - 1"
 
     elif player == "Paper":
         if computer == "Scissors":
             print("You lose!", computere, "cuts", player)
+            player_lives == "player_live - 1"
         else:
             print("You win!", player, "covers", computer)
+            computer_lives == "computer_lives - 1"
 
     elif player == "Scissors":
         if computer =="Rock":
             print("You lose!", computer, "smashes", player)
+            player_lives == "player_lives - 1"
         else:
             print("You win!", player, "cuts", computer)
+            computer_lives == "computer_lives - 1"
 
     elif player =="Quit":
         exit()
+    if lives =="0":
+        play_again = input("Play again?")
+        while play_again not in ("Yes","No"):  # validate user input
+            print("Please try again")
+            play_again = input("Play again?")
+        if play_again == "No":
+            print("Game Over")
+            continue_playing = False
+        # Don't return here (you returned the input), I really don't think that what you wanted
 
-    else:
         print("Not a valid option. Check again, and check you spelling\n")
 
     player = False
